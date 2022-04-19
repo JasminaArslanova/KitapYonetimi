eb6e28d251f5a98c8041c35c1e4b24900e5fea22
Jasmina Arslanova <zhasmina.arslanova@gmail.com>
2022-04-19 10:41:10 +0300

    1 commit

--- src/KitapYonetimi/Kitap.java
+++ src/KitapYonetimi/Kitap.java
@@ -1,5 +1,10 @@
 package KitapYonetimi;
 
 public class Kitap {
+    /*
+        1.olarak her kitaba ait kitap no olacak, sonra kitap adi olacak, buna ait
+		 * yazar adi olacak ve kitap fiyati olacak bu bilgilerin tutulacagi bir yer
+		 * olmali yani bir list'te tutmamiz gerek
+     */
 
 }
