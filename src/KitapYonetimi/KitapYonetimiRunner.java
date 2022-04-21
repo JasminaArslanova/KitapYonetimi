6cf15e54aec07565094a755329459df2f30d2310
sevil <sevilayturanuyanik@gmail.com>
2022-04-21 20:21:05 +0300

    21.04

--- src/KitapYonetimi/KitapYonetimiRunner.java
+++ src/KitapYonetimi/KitapYonetimiRunner.java
@@ -1,7 +1,13 @@
 package KitapYonetimi;
 
+import static KitapYonetimi.Kitap.KitapEkle;
+
+
 public class KitapYonetimiRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) {KitapEkle();
+
+
 
     }
+
 }
